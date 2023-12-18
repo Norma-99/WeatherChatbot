9a0d7a7eef231e2fe1fecc06e2508d48bbeb73ce
Norma-99 <normaescg@gmail.com>
2023-12-18 08:31:42 +0100

    Added model that works with 1 slot

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ import asyncio
 
 
 class ChatbotController:
-    def __init__(self, model_path="./models/20231217-110727-terminal-century.tar.gz"):
+    def __init__(self, model_path="./models/20231218-082112-open-edging.tar.gz"):
         """initialize the chatbot framework"""
         self.agent = Agent.load(model_path)
         print(f"Model: {self.agent}")
